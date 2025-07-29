554c000b859b106d0aca623dc775f52b76c3d792
Shubham kumar <shubhamdavid4606@gmail.com>
2025-07-29 18:04:19 +0530

    Tailwind

---
+++ ReactJS/day19/src/App.js
@@ -0,0 +1,148 @@
+import React, {useState} from "react";
+import ReactDOM from "react-dom/client"
+
+
+function App(){
+
+    
+    return(
+        <div className="flex flex-wrap justify-center gap-10">
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img className="w-full h-80 object-cover rounded-2xl"src="https://www.tribuneindia.com/sortd-service/imaginary/v22-01/jpg/large/high?url=dGhldHJpYnVuZS1zb3J0ZC1wcm8tcHJvZC1zb3J0ZC9tZWRpYTg5MDFkOTcwLTgwNTUtMTFlZi1iZjU3LTYzYWRhZWQ5ZDA2YS5qcGc="></img>
+
+            </div>
+        <div className="mt-2 font-bold text-2xl text-gray-700">
+
+            <h1>Biography</h1>
+
+        </div>
+        <div className="text-gray-300">
+
+            <p>Rohit is good boy</p>
+        </div>
+        <div className="mt-3 flex justify-center">
+            <button className="bg-yellow-200 p-1 rounded-xl text-white text-2xl hover:bg-yellow-800 ">Know more</button>
+        </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img className="w-full h-80 object-cover rounded-2xl"src="https://www.tribuneindia.com/sortd-service/imaginary/v22-01/jpg/large/high?url=dGhldHJpYnVuZS1zb3J0ZC1wcm8tcHJvZC1zb3J0ZC9tZWRpYTg5MDFkOTcwLTgwNTUtMTFlZi1iZjU3LTYzYWRhZWQ5ZDA2YS5qcGc="></img>
+
+            </div>
+        <div className="mt-2 font-bold text-2xl text-gray-700">
+
+            <h1>Biography</h1>
+
+        </div>
+        <div className="text-gray-300">
+
+            <p>Rohit is good boy</p>
+        </div>
+        <div className="mt-3 flex justify-center">
+            <button className="bg-yellow-200 p-1 rounded-xl text-white text-2xl hover:bg-yellow-800 ">Know more</button>
+        </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img className="w-full h-80 object-cover rounded-2xl"src="https://www.tribuneindia.com/sortd-service/imaginary/v22-01/jpg/large/high?url=dGhldHJpYnVuZS1zb3J0ZC1wcm8tcHJvZC1zb3J0ZC9tZWRpYTg5MDFkOTcwLTgwNTUtMTFlZi1iZjU3LTYzYWRhZWQ5ZDA2YS5qcGc="></img>
+
+            </div>
+        <div className="mt-2 font-bold text-2xl text-gray-700">
+
+            <h1>Biography</h1>
+
+        </div>
+        <div className="text-gray-300">
+
+            <p>Rohit is good boy</p>
+        </div>
+        <div className="mt-3 flex justify-center">
+            <button className="bg-yellow-200 p-1 rounded-xl text-white text-2xl hover:bg-yellow-800 ">Know more</button>
+        </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img className="w-full h-80 object-cover rounded-2xl"src="https://www.tribuneindia.com/sortd-service/imaginary/v22-01/jpg/large/high?url=dGhldHJpYnVuZS1zb3J0ZC1wcm8tcHJvZC1zb3J0ZC9tZWRpYTg5MDFkOTcwLTgwNTUtMTFlZi1iZjU3LTYzYWRhZWQ5ZDA2YS5qcGc="></img>
+
+            </div>
+        <div className="mt-2 font-bold text-2xl text-gray-700">
+
+            <h1>Biography</h1>
+
+        </div>
+        <div className="text-gray-300">
+
+            <p>Rohit is good boy</p>
+        </div>
+        <div className="mt-3 flex justify-center">
+            <button className="bg-yellow-200 p-1 rounded-xl text-white text-2xl hover:bg-yellow-800 ">Know more</button>
+        </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img className="w-full h-80 object-cover rounded-2xl"src="https://www.tribuneindia.com/sortd-service/imaginary/v22-01/jpg/large/high?url=dGhldHJpYnVuZS1zb3J0ZC1wcm8tcHJvZC1zb3J0ZC9tZWRpYTg5MDFkOTcwLTgwNTUtMTFlZi1iZjU3LTYzYWRhZWQ5ZDA2YS5qcGc="></img>
+
+            </div>
+        <div className="mt-2 font-bold text-2xl text-gray-700">
+
+            <h1>Biography</h1>
+
+        </div>
+        <div className="text-gray-300">
+
+            <p>Rohit is good boy</p>
+        </div>
+        <div className="mt-3 flex justify-center">
+            <button className="bg-yellow-200 p-1 rounded-xl text-white text-2xl hover:bg-yellow-800 ">Know more</button>
+        </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img className="w-full h-80 object-cover rounded-2xl"src="https://www.tribuneindia.com/sortd-service/imaginary/v22-01/jpg/large/high?url=dGhldHJpYnVuZS1zb3J0ZC1wcm8tcHJvZC1zb3J0ZC9tZWRpYTg5MDFkOTcwLTgwNTUtMTFlZi1iZjU3LTYzYWRhZWQ5ZDA2YS5qcGc="></img>
+
+            </div>
+        <div className="mt-2 font-bold text-2xl text-gray-700">
+
+            <h1>Biography</h1>
+
+        </div>
+        <div className="text-gray-300">
+
+            <p>Rohit is good boy</p>
+        </div>
+        <div className="mt-3 flex justify-center">
+            <button className="bg-yellow-200 p-1 rounded-xl text-white text-2xl hover:bg-yellow-800 ">Know more</button>
+        </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img className="w-full h-80 object-cover rounded-2xl"src="https://www.tribuneindia.com/sortd-service/imaginary/v22-01/jpg/large/high?url=dGhldHJpYnVuZS1zb3J0ZC1wcm8tcHJvZC1zb3J0ZC9tZWRpYTg5MDFkOTcwLTgwNTUtMTFlZi1iZjU3LTYzYWRhZWQ5ZDA2YS5qcGc="></img>
+
+            </div>
+        <div className="mt-2 font-bold text-2xl text-gray-700">
+
+            <h1>Biography</h1>
+
+        </div>
+        <div className="text-gray-300">
+
+            <p>Rohit is good boy</p>
+        </div>
+        <div className="mt-3 flex justify-center">
+            <button className="bg-yellow-200 p-1 rounded-xl text-white text-2xl hover:bg-yellow-800 ">Know more</button>
+        </div>
+        </div>
+        
+        </div>
+    )
+
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>);
